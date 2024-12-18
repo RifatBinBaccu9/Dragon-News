159467bcc49e3e4f2fbcd748f69cff9cbb3248b1
RifatBinBaccu9 <rifatbinbaccu@gmail.com>
2024-12-18 12:59:14 +0600

    first commit

---
+++ src/Firebase/Firebase.config.js
@@ -0,0 +1,19 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyDS7J2S3RZ3nU5lm-GETI-4k5kyAKticFA",
+  authDomain: "dragon-news-resources-4bc70.firebaseapp.com",
+  projectId: "dragon-news-resources-4bc70",
+  storageBucket: "dragon-news-resources-4bc70.firebasestorage.app",
+  messagingSenderId: "659653242809",
+  appId: "1:659653242809:web:7a7524277ca1370b7a9068"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+export default app;
---
+++ src/Pages/Home/Home.jsx
@@ -0,0 +1,12 @@
+import Header from "../Shahed/Header/Header";
+
+
+const Home = () => {
+    return (
+        <div>
+            <Header />
+        </div>
+    );
+};
+
+export default Home;
